ccb979d35a7680571ea8db440cfc5cd22cec94cc
svarun115 <svarun94@gmail.com>
2015-08-29 10:21:22 +0530

    preprocessing

---
+++ preprocessing.py
@@ -0,0 +1,51 @@
+from nltk import sent_tokenize
+from nltk.tokenize import RegexpTokenizer
+from nltk.stem.lancaster import LancasterStemmer
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+import pickle
+import numpy as np
+import re
+
+def derive_vocabulary(filename):
+	stemmed = []
+	text = None
+	with open(filename,"r") as f:
+		text = f.read()
+	text = eval(text)
+	documents = {}
+	for doc,index in zip(text,range(len(text))):
+		query = process_query(doc,False)
+		documents[index] = query
+	with open('documents.pkl', 'wb') as output:
+		pickle.dump(documents, output, pickle.HIGHEST_PROTOCOL)
+	return documents
+
+def stopword(word_list):
+    filtered_words = [w for w in word_list if not w in stopwords.words('english')]
+    return filtered_words
+
+
+def process_query(query,stem=True):
+	st = LancasterStemmer()
+	re_tokenizer = RegexpTokenizer('\w+')
+	words = re_tokenizer.tokenize(query)
+	wnl = WordNetLemmatizer()
+	stemmed = []
+	if stem :
+		for i in words:
+			stemmed.append(st.stem(i.decode("utf-8").lower()))
+	else:
+		for i in words:
+			stemmed.append(wnl.lemmatize(i.decode("utf-8").lower()))
+	stemmed = stopword(stemmed)
+	return stemmed
+
+
+def append_query(query):
+		q = process_query(query)
+		
+
+
+documents = derive_vocabulary("Linear.txt")
+print documents
